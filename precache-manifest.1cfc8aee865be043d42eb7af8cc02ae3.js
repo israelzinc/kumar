242ac5555a40ec438c00fc4412935ae0219cc078
Starlord <israelzinc@Zincs-MacBook-Pro.local>
2020-07-25 15:08:50 +0900

    Updates

---
+++ precache-manifest.1cfc8aee865be043d42eb7af8cc02ae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e67e3d3d141985e2effa14fabb86be",
+    "url": "/kumar/index.html"
+  },
+  {
+    "revision": "633b3a27c05f91d85d63",
+    "url": "/kumar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dca7f6291fcd2b646328",
+    "url": "/kumar/static/js/2.93e4c8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kumar/static/js/2.93e4c8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633b3a27c05f91d85d63",
+    "url": "/kumar/static/js/main.a7d016fa.chunk.js"
+  },
+  {
+    "revision": "6bcfee1c9ad587f8caa5",
+    "url": "/kumar/static/js/runtime-main.51d1b9dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kumar/static/media/logo.5d5d9eef.svg"
+  }
+]);
